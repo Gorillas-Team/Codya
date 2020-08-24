1d6934cae2093e2d0ec4a4b3fc9c266c7d80a89b
zMigueel <miguel2345.mdmp@gmail.com>
2020-08-24 20:08:39 -0300

    Xp model refactor

--- src/listeners/client/MessageListener.js
+++ src/listeners/client/MessageListener.js
@@ -3,23 +3,26 @@ const { getPrefix } = require('../../utils')
 const xpCooldown = new Set()
 
 module.exports = class MessageListener extends Listener {
-  constructor() {
+  constructor () {
     super({
       name: 'message'
     })
   }
 
-  async run(message) {
+  async run (message) {
     if (message.author.bot || message.channel.type === 'dm') return
 
     const userDocument = await message.author.document
 
     if (!xpCooldown.has(message.author.id)) {
       const { xp, level } = userDocument
+      const queryOptions = { _id: message.author.id }
 
-      await userDocument.update({ xp: Math.floor(Math.random() * 5) + 5 })
+      this.database.models.users.updateOne(queryOptions, { xp: Math.floor(Math.random() * 3) + 2 })
 
-      if (xp >= level * 60) await userDocument.update({ xp: 0, level: xp / 60 - level })
+      if (xp >= level * 60) {
+        await this.database.models.users.updateOne(queryOptions, { xp: 0, $inc: { level: Math.floor(xp / 60 - level) } })
+      }
 
       xpCooldown.add(message.author.id)
     }
