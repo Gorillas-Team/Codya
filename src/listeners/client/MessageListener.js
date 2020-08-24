be23aa502f61c18c1d0da0f6761dfe9306d4fbab
zMigueel <miguel2345.mdmp@gmail.com>
2020-08-24 19:31:40 -0300

    Xp model refactor

--- src/listeners/client/MessageListener.js
+++ src/listeners/client/MessageListener.js
@@ -1,17 +1,30 @@
 const { Listener, CommandContext } = require('../../structures/client')
 const { getPrefix } = require('../../utils')
+const xpCooldown = new Set()
 
 module.exports = class MessageListener extends Listener {
-  constructor () {
+  constructor() {
     super({
       name: 'message'
     })
   }
 
-  async run (message) {
+  async run(message) {
     if (message.author.bot || message.channel.type === 'dm') return
 
-    message.guild.data = await this.database.findDocument(message.guild.id, 'guilds')
+    const userDocument = await message.author.document
+
+    if (!xpCooldown.has(message.author.id)) {
+      const { xp, level } = userDocument
+
+      await userDocument.update({ xp: Math.floor(Math.random() * 5) + 5 })
+
+      if (xp >= level * 60) await userDocument.update({ xp: 0, level: xp / 60 - level })
+
+      xpCooldown.add(message.author.id)
+    }
+
+    setTimeout(() => xpCooldown.delete(message.author.id), 60000)
 
     const prefix = getPrefix(message)
     if (!message.content.toLowerCase().startsWith(prefix)) return
